9242852afa9c264c1ddef0c899fb2d00effea444
CSD <csd@minemelon.net>
2025-04-18 10:33:01 +0700

    Update gradle.properties with the latest commit from Winds-Studio/Leaf

--- scripts/upstream.sh
+++ scripts/upstream.sh
@@ -3,13 +3,24 @@
 # requires curl
 
 # Fetch the latest commit hash from Winds-Studio/Leaf and update gradle.properties
-curl -s "https://api.github.com/repos/Winds-Studio/Leaf/commits" | \
+commitHash=$(curl -s "https://api.github.com/repos/Winds-Studio/Leaf/commits" | \
 grep -o '"sha": "[a-f0-9]\{40\}"' | \
 head -n 1 | \
 awk -F': ' '{print $2}' | \
-tr -d '"' | \
-xargs -I {} sed -i "s/^Commit =.*/Commit = {}/" gradle.properties
+tr -d '"')
 
-# Commit the change
-git add gradle.properties
-git commit -m "Update gradle.properties with the latest commit from Winds-Studio/Leaf"
+# Kiểm tra xem có commit hash hay không
+if [ -n "$commitHash" ]; then
+    # Chuyển file gradle.properties về định dạng Unix (LF) nếu nó đang có định dạng Windows (CRLF)
+    dos2unix gradle.properties
+
+    # Cập nhật gradle.properties với commit hash mới nhất
+    sed -i "s/^-Commit =.*/-Commit = $commitHash/" gradle.properties
+
+    # Commit thay đổi
+    git add gradle.properties
+    git commit -m "Update gradle.properties with the latest commit from Winds-Studio/Leaf"
+else
+    echo "Failed to fetch the latest commit hash."
+    exit 1
+fi
